f46733b135d35498526b5f7a6a94ad08c87a64b3
sevenc-nanashi <sevenc7c@sevenc7c.com>
2022-08-12 15:30:44 +0900

    Add: Add Japanese translation

--- src/i18n.ts
+++ src/i18n.ts
@@ -12,4 +12,17 @@ export default {
       log: "Log",
     },
   },
+  ja: {
+    about: {
+      main: `Sonolus 0.5.13で作られたSCP (Sonolus Collection Package)を0.6.0で読み込めるようにするアプリ。ファイルはサーバーに送信{0}すべての作業はローカルで行われます。`,
+      notSent: "されません。",
+      madeBy: "開発：{0}",
+      sevenc7c: "名無し｡ ({'@'}sevenc-nanashi)",
+    },
+    app: {
+      convert: "変換",
+      download: "ダウンロード：{name}",
+      log: "ログ",
+    },
+  },
 }
